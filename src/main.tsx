13c28a96e6a8890ab9ab75f00146900fb23753ea
michaelsowka <michaelsznipe@gmail.com>
2025-09-07 01:42:22 +0200

    Navbar Design

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,22 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Charging from './pages/Charging';
 
+import './css/Navbar.css';
+
+{/* @ts-ignore */}
+import homeIcon from './images/home.png';
+{/* @ts-ignore */}
+import profileIcon from './images/profile.png';
+{/* @ts-ignore */}
+import micIcon from './images/mic.png';
+{/* @ts-ignore */}
+import disconnectIcon from './images/disconnect.png';
+
 const App = () => {
   const [currentUrl, setCurrentUrl] = useState<string>('/');
+
+  //TS will complain if Link isn't cast to any. A library that would fix this is deprecated.
+  const AnyLink = Link as any;
 
   return (
     <div class="app">
@@ -18,13 +32,43 @@ const App = () => {
         <Route path="/charging" component={Charging} />
       </Router>
 
-      {/* Show navbar on every page except "/" */}
       {currentUrl !== '/' && (
         <nav class="navbar">
-          {/* @ts-ignore */}
-          <Link href="/profile" class="nav-button">Profile</Link>
-          {/* @ts-ignore */}
-          <Link href="/charging" class="nav-button">Charging</Link>
+          {/* Slot 1: Profile */}
+          <AnyLink
+            href="/profile"
+            class={`nav-button ${currentUrl === '/profile' ? 'active' : ''}`}
+          >
+            <img src={profileIcon} class="nav-icon" alt="Profile" />
+            <span>PROFILE</span>
+          </AnyLink>
+
+          {/* Slot 2: Disconnect */}
+          <AnyLink
+            href="/disconnect"
+            class={`nav-button ${currentUrl === '/disconnect' ? 'active' : ''}`}
+          >
+            <img src={disconnectIcon} class="nav-icon" alt="Disconnect" />
+            <span>DISCONNECT</span>
+          </AnyLink>
+
+          {/* Slot 3: Mic */}
+          <AnyLink
+            href="/mic"
+            class={`nav-button ${currentUrl === '/mic' ? 'active' : ''}`}
+          >
+            <img src={micIcon} class="nav-icon" alt="Mic" />
+            <span>ELLIOTH</span>
+          </AnyLink>
+
+          {/* Slot 4: Charging */}
+          <AnyLink
+            href="/charging"
+            class={`nav-button ${currentUrl === '/charging' ? 'active' : ''}`}
+          >
+            <img src={homeIcon} class="nav-icon" alt="Home" />
+            <span>HOME</span>
+          </AnyLink>
         </nav>
       )}
     </div>
