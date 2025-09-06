e1f629f8c9013fb47313582bd059a83a31737d8a
michaelsowka <michaelsznipe@gmail.com>
2025-09-06 04:13:45 +0200

    Basic Linking

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,30 @@
+// src/main.tsx
 import { render } from 'preact';
 import { Router, Route } from 'preact-router';
+import { Link } from 'preact-router';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Charging from './pages/Charging';
 
-const root = document.getElementById('react-root');
-if (root) {
-  render(
+const App = () => (
+  <div class="app">
     <Router>
       <Route path="/" component={Home} />
       <Route path="/profile" component={Profile} />
-    </Router>,
-    root
-  );
+      <Route path="/charging" component={Charging} />
+    </Router>
+
+    {/* Bottom navbar */}
+    <nav class="navbar">
+      {/* @ts-ignore */}
+      <Link href="/profile" class="nav-button">Profile</Link>
+      {/* @ts-ignore */}
+      <Link href="/charging" class="nav-button">Charging</Link>
+    </nav>
+  </div>
+);
+
+const root = document.getElementById('react-root');
+if (root) {
+  render(<App />, root);
 }
---
+++ src/pages/Charging.tsx
@@ -0,0 +1,8 @@
+export default function Charging() {
+  return (
+    <div>
+      <h1>Charging</h1>
+      <p>This is the charging page.</p>
+    </div>
+  );
+}
